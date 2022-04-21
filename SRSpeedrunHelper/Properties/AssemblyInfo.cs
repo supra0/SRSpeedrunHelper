7826962f49ad802c2fb1370e272320a50211e5ea
Supra <supramayro@gmail.com>
2022-04-20 21:30:21 -0700

    Minor description change

--- SRSpeedrunHelper/Properties/AssemblyInfo.cs
+++ SRSpeedrunHelper/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("SR Speedrun Helper")] //Set this to the full name of the mod including spaces.
 [assembly: AssemblyTitle("SRSpeedrunHelper")] //This is only used when mousing over a dll file in Windows explorer.
-[assembly: AssemblyDescription("A Slime Rancher mod for speedrunning research and routing")]
+[assembly: AssemblyDescription("A Slime Rancher mod for speedrunning practice, research, and routing.")]
 [assembly: AssemblyCompany("https://github.com/supra0/SRSpeedrunHelper")] //Set this to your name/nickname and/or website
 [assembly: AssemblyVersion("1.1.1")] //Set this to the version of your mod.
 [assembly: AssemblyFileVersion("1.1.1")] //Set this to the version of your mod.
